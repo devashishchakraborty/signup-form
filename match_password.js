f51463d64ae843a2903f75f599b63ee8c3a1fa0a
Devashish Chakraborty <devashishchakra@gmail.com>
2023-01-29 21:46:23 +0530

    Added some conditional statements to check whether the passwords match. If both fields are empty then return to their original state with grey border

--- match_password.js
+++ match_password.js
@@ -4,16 +4,28 @@ const confirmPassword = document.querySelector("input#confirm-password")
 let passwordValue = "";
 let confirmPasswordValue = "";
 
-password.addEventListener("focus", function(){
-    password.addEventListener("keyup", function(){
+password.addEventListener("focus", function () {
+    password.addEventListener("keyup", function () {
         passwordValue = password.value;
-        console.log(passwordValue === confirmPasswordValue);
+        if (passwordValue.length === 0 && confirmPasswordValue.length === 0) {
+            password.setAttribute("matched", "");
+            confirmPassword.setAttribute("matched", "");
+        }
     })
 });
 
-confirmPassword.addEventListener("focus", function(){
-    confirmPassword.addEventListener("keyup", function(){
+confirmPassword.addEventListener("focus", function () {
+    confirmPassword.addEventListener("keyup", function () {
         confirmPasswordValue = confirmPassword.value;
-        console.log(passwordValue === confirmPasswordValue);
+        if (passwordValue.length === 0 && confirmPasswordValue.length === 0) {
+            password.setAttribute("matched", "");
+            confirmPassword.setAttribute("matched", "");
+        } else if (passwordValue === confirmPasswordValue) {
+            password.setAttribute("matched", "true");
+            confirmPassword.setAttribute("matched", "true");
+        } else {
+            password.setAttribute("matched", "false");
+            confirmPassword.setAttribute("matched", "false");
+        }
     })
 });
