8fcfaca1688418168fff900b685fd94d9a98662b
Devashish Chakraborty <devashishchakra@gmail.com>
2023-01-29 21:16:27 +0530

    Storing Value of password in variable when focused on the password element

--- match_password.js
+++ match_password.js
@@ -3,3 +3,10 @@ const confirmPassword = document.querySelector("input#confirm-password")
 
 let passwordValue = "";
 let confirmPasswordValue = "";
+
+password.addEventListener("focus", function(){
+    password.addEventListener("keyup", function(){
+        passwordValue = password.value;
+        console.log(passwordValue === confirmPasswordValue);
+    })
+});
