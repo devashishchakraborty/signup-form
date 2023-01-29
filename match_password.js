e0ad115c3837d193ddead143cb0cb0c0e9741c4c
Devashish Chakraborty <devashishchakra@gmail.com>
2023-01-29 21:14:37 +0530

    declared variables and Objects for password texts and form elements respectively

---
+++ match_password.js
@@ -0,0 +1,5 @@
+const password = document.querySelector("input#password")
+const confirmPassword = document.querySelector("input#confirm-password")
+
+let passwordValue = "";
+let confirmPasswordValue = "";
